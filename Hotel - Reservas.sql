3cde82e2f047b315f8605f7b0ee2fb13a89d43d9
Caio Ramos de Lima <112398430+CaioRDL@users.noreply.github.com>
2023-05-17 00:31:39 -0300

    Update Hotel - Reservas.sql

--- Hotel - Reservas.sql
+++ Hotel - Reservas.sql
@@ -1,14 +1,16 @@
+#Base de Dados para realizar reservas em um Hotel
+
+
 #Primeiro vamos criar uma central de dados
-drop database hotel;
 create database hotel;
-#Após, vamos usar esse banco e crear as nossas tabelas
+#Após, vamos usar esse banco e criar as nossas tabelas
 use hotel;
 #Criando a tabela cliente.
 create table cliente(
 		id_cliente INT NOT NULL PRIMARY KEY AUTO_INCREMENT, #Aqui declaramos que o id_cliente é uma chave primaria, automaticamente preenchido e não pode ser nulo
         nome varchar (255) not null, 
         sobrenome varchar(255) not null,
-        tipo_documento enum("cpf","rg","passaporte") not null,
+        tipo_documento enum("cpf","rg","passaporte") not null, #Declarei como "ENUM", para que a coluna tipo_de_documento, só aceite aqueles documentos 
         endereco varchar (255) not null,
         data_registro datetime default now() #Registro auto preenchido.
         );
@@ -21,16 +23,18 @@ create table reservas(
         observacao text,
         id_cliente int,
         foreign key(id_cliente) references cliente(id_cliente)#Aqui indicamos que as tabelas fazem referencia pelo id.
+	#Essas tabelas que fazem referencia pelo id, será de grande necessidade no final de nosso código.
         );
 
 #Agora, vamos excluir a coluna observacao e adicionar a coluna comentario_cliente do tipo texto.
 ALTER TABLE reservas DROP COLUMN observacao;
-select * from reservas;
+select * from reservas;#Conferindo que foi deletado.
 ALTER TABLE reservas ADD COLUMN comentario_cliente varchar(255);
 
 
 #---------------------------------------------------------------------------------
-#Vamos fazer as atribuições  das colunas dentro das tabelas cliente e reservas.
+#Vamos fazer as atribuições  das colunas dentro das tabelas cliente e reservas utilizando o insert into.
+
 insert into cliente(nome,sobrenome,tipo_documento,endereco,data_registro)
 values ("Caio","Ramos de Lima","RG","Guilherme Ceolin",default);
         
@@ -40,9 +44,11 @@ values ("Djhonatan","Ramos de Lima","cpf","Uberaba",default);
 insert into cliente(nome,sobrenome,tipo_documento,endereco,data_registro)
 values ("Arthur","Ramos Lima","passaporte","Joao Kleber",default);
 
-select * from cliente;
+select * from cliente;#Conferindo tabela.
         
 #-------------------------------------------------------------------------------
+#Atribuições da tabela reservas.
+
  insert into reservas(data_entrada,data_saida,data_registro,comentario_cliente)
  values (default,"2023-04-22 18:30:30",default,"Muito bom");
 
@@ -63,7 +69,9 @@ values (default,"2023-04-22 18:30:30",default,"Extremamente extremo");
 
 
  
- select * from reservas;
+ select * from reservas; #Conferindo tabela
+ 
+ #Agora, vamos mudar o nome dos clientes pelo seu "id_cliente",
       update 
       cliente
      set
@@ -72,7 +80,8 @@ values (default,"2023-04-22 18:30:30",default,"Extremamente extremo");
      tipo_documento = "RG",
      endereco = "Olavio Bilack",
      data_registro = default
-     where id_cliente = "1";
+     #declaramos where para realizar uma condição
+     where id_cliente = "1"; 
      
      update 
       cliente
@@ -93,8 +102,10 @@ values (default,"2023-04-22 18:30:30",default,"Extremamente extremo");
      endereco = "Olavio Black",
      data_registro = default
      where id_cliente = "3";
-select * from cliente;
+     
+select * from cliente;#Conferindo tabela cliente
 
+#Vou realizar uma Subquery  para que eu possa realizar uma consultada filtrada com informações especificas
 select a.id_reserva,
 	(select nome 
 	from cliente as b
